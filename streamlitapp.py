fbcf0af0b868a3176645452a3a3fb6cf603c3ad9
sachin-h-s <89412649+sachin-h-s@users.noreply.github.com>
2021-09-02 23:29:57 +0530

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -59,7 +59,7 @@ def predict_note_authentication(variance,skewness,curtosis,entropy):
 
 
 def main():
-    st.title("Bank Authenticator")
+    st.title("Bank Note Authenticator")
     html_temp = """
     <div style="background-color:tomato;padding:10px">
     <h2 style="color:white;text-align:center;">Streamlit Bank Authenticator ML App </h2>
